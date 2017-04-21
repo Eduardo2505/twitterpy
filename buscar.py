11f76a2565c62bdb4d8dd25159a6c8b13a3b1bec
Eduardo2505 <eduardopadillacz@gamail.com>
2017-04-21 16:42:27 -0500

    Subir

--- buscar.py
+++ buscar.py
@@ -1,4 +1,3 @@
-
 from TwitterSearch import *
 try:
     tso = TwitterSearchOrder() # create a TwitterSearchOrder object
@@ -7,19 +6,18 @@ try:
     tso.set_geocode(19.350000,-99.150002,5,imperial_metric=True)
     tso.set_result_type('recent')
     tso.set_count(42)
-    #tso.set_include_entities(False) # and don't give us all those entity information
 
     # it's about time to create a TwitterSearch object with our secret tokens
     ts = TwitterSearch(
-    	consumer_key = 'aFXfsYucGeY1JDVYd0jZBQRDy',
-    	consumer_secret = 'w3ZFtpJiyYFz0aAUhJMc6pBK9bFDzkTWQGlo6thI9BkdmW9T84',
-    	access_token = '3033706820-ibVYRaDKIRlgsDDQQ0gwM0tb811FYjWNeH42LeC',
-    	access_token_secret = 'LsseJl4Xpnx8cT9ukAQtqM755YXz43lk3YcZTBRHcX1AD'
-    	)
+        consumer_key = 'aFXfsYucGeY1JDVYd0jZBQRDy',
+        consumer_secret = 'w3ZFtpJiyYFz0aAUhJMc6pBK9bFDzkTWQGlo6thI9BkdmW9T84',
+        access_token = '3033706820-ibVYRaDKIRlgsDDQQ0gwM0tb811FYjWNeH42LeC',
+        access_token_secret = 'LsseJl4Xpnx8cT9ukAQtqM755YXz43lk3YcZTBRHcX1AD'
+     )
 
      # this is where the fun actually starts :)
-        for tweet in ts.search_tweets_iterable(tso):
-     	   print( '@%s tweeted: %s' % ( tweet['user']['screen_name'], tweet['text'] ) )
+    for tweet in ts.search_tweets_iterable(tso):
+        print( '@%s tweeted: %s' % ( tweet['user']['screen_name'], tweet['text'] ) )
 
 except TwitterSearchException as e: # take care of all those ugly errors if there are some
-print(e)
+    print(e)
